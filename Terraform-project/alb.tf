bf984bc9d3cb130cf62822a5ff40b28f12a963f4
ArmineKhachatryan1982 <armine.khachatryan1982@gmail.com>
2024-03-30 08:19:58 +0400

    30.03.24

--- Terraform-project/alb.tf
+++ Terraform-project/alb.tf
@@ -9,7 +9,7 @@ resource "aws_lb" "test" {
 
 }
 #Creating target group
-resource "aws_lb_target_group" "test" {
+resource "aws_lb_target_group" "my_target_group" {
   name     = "tf-example-lb-tg"
   port     = 80
   protocol = "HTTP"
@@ -23,7 +23,12 @@ resource "aws_lb_listener" "front_end" {
  
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.test.arn
+    target_group_arn = aws_lb_target_group.my_target_group.arn
   }
+}
+# Attach Auto Scaling Group to Target Group
+resource "aws_autoscaling_attachment" "asg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.my_auto_scaling_group.name
+  lb_target_group_arn   = aws_lb_target_group.my_target_group.arn
 }
 
